c62c47ebafdc3a9b64d9324815264add222d0117
Matthew Reardon <patriotnewsactivism@gmail.com>
2026-01-23 20:13:26 -0600

    Update index.ts

--- supabase/functions/ocr-document/index.ts
+++ supabase/functions/ocr-document/index.ts
@@ -82,7 +82,7 @@ serve(async (req) => {
 
     const googleApiKey = Deno.env.get('GOOGLE_AI_API_KEY') || "AIzaSyAnNDe7iyPSbzPkhwo8pqKXvU7tsanbdHw";
     const supabaseUrl = Deno.env.get('SUPABASE_URL')!;
-    const serviceRoleKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY');
+    const serviceRoleKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') || "eyJhbGciOiJFUzI1NiIsImtpZCI6ImI4MTI2OWYxLTIxZDgtNGYyZS1iNzE5LWMyMjQwYTg0MGQ5MCIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MjA4NDUzNzY4Mn0.sZ9Z2QoERcdAxXInqq5YRpH5JLlv4Z8wqTz81X9gZ4Sah4w2XXINGPb8WQC5n3QsSHhKENOCgWOvqm3BD_61DA";
     const authHeader = req.headers.get('Authorization') || '';
 
     type AuthUser = { id: string };
