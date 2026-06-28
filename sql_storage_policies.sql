552f20330ddfe86f28d1270b04513418a1d50700
Matthew Reardon <patriotnewsactivism@proton.me>
2026-06-28 13:35:19 -0500

    a

--- sql_storage_policies.sql
+++
@@ -1,3 +0,0 @@
-CREATE POLICY "case_documents_insert_own" ON storage.objects FOR INSERT TO authenticated WITH CHECK (bucket_id = 'case-documents' AND auth.uid()::text = (storage.foldername(name))[1]);
-CREATE POLICY "case_documents_update_own" ON storage.objects FOR UPDATE TO authenticated USING (bucket_id = 'case-documents' AND auth.uid()::text = (storage.foldername(name))[1]) WITH CHECK (bucket_id = 'case-documents' AND auth.uid()::text = (storage.foldername(name))[1]);
-CREATE POLICY "case_documents_delete_own" ON storage.objects FOR DELETE TO authenticated USING (bucket_id = 'case-documents' AND auth.uid()::text = (storage.foldername(name))[1]);
